401819cc63ead97dcd351da33ab0ccf7c16cffe0
chris23insa <chris23insa@users.noreply.github.com>
2019-11-12 15:23:08 +0100

    repo creation + navbar implemented

---
+++ app/src/main/java/com/example/chris/travelorga_kth/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.chris.travelorga_kth;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        /**
+         * Do something when the item is selected
+         *
+         * @param item
+         * @return
+         */
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.action_trips:
+                    return true;
+                case R.id.action_search:
+                    return true;
+                case R.id.action_profile:
+                    return true;
+                case R.id.action_map:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationViewHelper.removeShiftMode(navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
